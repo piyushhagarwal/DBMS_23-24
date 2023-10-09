4ea94713bc9c4b153d3adf48635fad8dcf8315d2
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-10-09 11:53:55 +0530

    Initial Folder structure

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+// extra packages
+
+// routes
+
+const port = process.env.PORT || 8080;
+
+const start = async () => {
+  try {
+    // await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ...${port}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
